1ea7f463c26f2dc4cf0dba62a38e91a46d12fcb3
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-14 22:26:21 +0100

    created class, arraylist and method to add multiple string to list

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Playlist {
+    public static void main(String[] args) {
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+        createPlaylist(desertIslandPlaylist, "desert music", "pyramid music", "beach music", "island music", "sleep music");
+    }
+
+    public static void createPlaylist(ArrayList<String> arrayList, String title1, String title2, String title3, String title4, String title5){
+        arrayList.add(title1);
+        arrayList.add(title2);
+        arrayList.add(title3);
+        arrayList.add(title4);
+        arrayList.add(title5);
+    }
 }
