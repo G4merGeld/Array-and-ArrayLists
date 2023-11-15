9e44128218810fe8dfdbca88eaa333f8c439001b
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-15 02:57:13 +0100

    added shuffle method

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,11 +1,13 @@
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Random;
 
 public class Playlist {
     public static void main(String[] args) {
         ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
-        createPlaylist(desertIslandPlaylist, "desert music", "pyramid music", "\"change me\" music", "island music", "sleep music", "boring music");
+        Playlist.createPlaylist(desertIslandPlaylist, "desert music", "pyramid music", "\"change me\" music", "island music", "sleep music", "boring music");
         System.out.println(desertIslandPlaylist);
 
         //get length/size of list
@@ -29,14 +31,17 @@ public class Playlist {
         //reverse list
         Collections.reverse(desertIslandPlaylist);
         System.out.println(desertIslandPlaylist);
-        //oder reverse list (siehe unten)
+        //or reverse list (see more further down)
         reversePlaylist(desertIslandPlaylist);
         System.out.println(desertIslandPlaylist);
 
+        //shuffle playlist
+        shufflePlaylist(desertIslandPlaylist);
+        System.out.println(desertIslandPlaylist);
     }
 
     //adding values to ArrayLists in order or by adding index
-    public static void createPlaylist(ArrayList<String> playlist, String title1, String title2, String title3, String title4, String title5 , String title6) {
+    public static void createPlaylist(ArrayList<String> playlist, String title1, String title2, String title3, String title4, String title5, String title6) {
         playlist.add(title1);
         playlist.add(title2);
         playlist.add(title3);
@@ -57,6 +62,25 @@ public class Playlist {
             }
         } else {
             System.out.println("Playlist is empty!");
+        }
+    }
+    //shuffle list
+    public static void shufflePlaylist(ArrayList<String> playlist){
+        Random rand = new Random(); //create random
+        ArrayList<String> copy = (ArrayList<String>) playlist.clone(); //create copy of playlist
+        ArrayList<Integer> randomPositions = new ArrayList<Integer>(); //create list to save random numbers
+        int i = 0;
+        //System.out.println(copy);
+        while (i < playlist.size()){
+            int position = rand.nextInt(playlist.size()); //create random number between 0 (inclusive) and(playlist.size (exclusive)
+            if (!randomPositions.contains(position)) { //check if number was already generated before
+                randomPositions.add(position); //add number to list
+                i++;
+            }
+        }
+        //System.out.println(randomPositions);
+        for (int j = 0; j < playlist.size(); j++) {
+            playlist.set(j, copy.get(randomPositions.get(j))); //set playlist with random songs in order
         }
     }
 }
